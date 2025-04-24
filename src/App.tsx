1142153a3b195db0934f8cc9075de288b10c2fdb
Rafal Wizen <rafal.wizen@gmail.com>
2025-04-25 00:32:17 +0200

    Fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
+import router from './router';
 import { AuthProvider } from './contexts/AuthContext';
 import { ToastProvider } from './contexts/ToastContext';
 import ToastContainer from './components/ui/ToastContainer';
